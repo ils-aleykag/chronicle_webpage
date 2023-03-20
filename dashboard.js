060bc172220bf1eea81c6e6a148c9eb480bfe119
ils-aleykag <a.gazdhar@thesynapses.com>
2023-03-20 17:32:48 +0530

    dashboard-chart

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,4 @@
 // SIDEBAR TOGGLE
-
 var sidebarOpen = false;
 var sidebar = document.getElementById("sidebar");
 
@@ -21,3 +20,43 @@ function closeSidebar() {
 
 // ---------- CHARTS ----------
 
+// -------- bar chart --------
+var xValues = ["School Name 01", "School Name 02", "School Name 03", "School Name 04"];
+
+var barColors = "#F9AC32";
+
+new Chart("bar-chart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data:[150, 210, 350, 250],
+      barThickness: 20,
+      label: "Active"
+    },
+    {
+      backgroundColor: "#EEEEEE",
+      data: [200, 300, 400, 300],
+      barThickness: 20,
+      label: "Inactive",
+    }]
+  },
+
+  options: {
+    legend: {
+      display: true,
+    },
+
+    scales: {
+       x:  {
+        stacked:true,
+      },
+  
+      y: {
+        stacked:true,
+      },
+    },
+     
+  },
+});
