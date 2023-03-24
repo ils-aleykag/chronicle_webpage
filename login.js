8b12067eb095c22a57c79906826b0ae2e6cd4e84
ils-aleykag <a.gazdhar@thesynapses.com>
2023-03-24 23:51:02 +0530

    cookies_added

--- login.js
+++ login.js
@@ -26,7 +26,7 @@ function changeImage() {
 
 function validateEmail(){
     var email = document.getElementById('contact-email').value;
-    sessionStorage.setItem('EmailId', email);
+   // sessionStorage.setItem('EmailId', email);
     var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;  //Javascript reGex for Email Validation.
     if(email.length == ""){
         Emailerr.style.visibility="visible";
@@ -48,7 +48,7 @@ return true;
 }
 function validatePswd(){
     var password = document.getElementById('contact-password').value;
-    sessionStorage.setItem('Password', password);
+    //sessionStorage.setItem('Password', password);
     console.log(password);
     if(password.length == 0){
         Passerr.innerHTML=' *Please enter your password';
@@ -82,6 +82,72 @@ function validateForm(){
     }
     return true;
 }
+
+/****cookies****/
+
+function setCookie() {
+    //fetch values from input box//
+    var  email= document.getElementById("contact-email").value
+    var password = document.getElementById("contact-password").value
+    var rememberMe = document.getElementById("flexCheckChecked").checked
+
+    // set cookies where path is defined//
+    if(rememberMe == true){
+        document.cookie = "email="+email+";path=http://127.0.0.1:5500/"
+        document.cookie = "password="+password+";path=http://127.0.0.1:5500/"
+        document.cookie = "rememberMe="+rememberMe+";path=http://127.0.0.1:5500/"
+    }else{
+        //max-age=max-age-in-seconds (e.g., 606024*365 or 31536000 for a year) ;expires=date-in-GMTString-format If neither expires nor max-age specified it will expire at the end of session.
+        document.cookie = "email=;  expires=Fri, 31 Dec 9999 23:59:59 GMT; path=http://127.0.0.1:5500/;";
+        document.cookie = "password=; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=http://127.0.0.1:5500/;";
+        document.cookie = "rememberMe=; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=http://127.0.0.1:5500/;";
+    }
+
+}
+
+//this will fetch data from cookies to display in inputbox//
+function getCookieData() {
+    console.log(document.cookie);
+    var email = getCookie("email");
+    var password = getCookie("password");
+    var rememberMe = getCookie("rememberMe");
+    document.getElementById("contact-email").value = email;
+    document.getElementById("contact-password").value = password;
+    document.getElementById("flexCheckChecked").checked = rememberMe;
+
+}
+
+//cookies save data in key-value pair in form of string, so to get indiviual field data, used getCookie function//
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  } 
+
+  function validateForm(){
+    if(!validateEmail() || !validatePswd()){
+         //loginBtn.classList.add("disable");
+         card.style.Height="500px";
+        //Suberr.innerHTML='*Please enter details in all feilds to submit';
+        return false;
+    }
+    else{
+       // window.alert("LogedIn successfully!");
+        window.location.href = "dashboard.html";
+    }
+    return true;
+}
+
 
 
 
