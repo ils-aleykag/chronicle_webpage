adee1cfbbb3628cb5e27195289d6933f7bc681b7
ils-aleykag <a.gazdhar@thesynapses.com>
2023-04-28 11:49:16 +0530

    SessionAuth

--- LMS/api/views.py
+++ LMS/api/views.py
@@ -1,6 +1,8 @@
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 # from rest_framework.filters import OrderingFilter
 
 
@@ -9,6 +11,9 @@ class BookModelViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filterset_fields = ['bookname', 'status_of_book', 'author']
+    authetication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
+    # permission_classes = [IsAuthenticated/AllowAny/IsAdminUser(staff true ones)/ IsAuthenticatedOrReadOnly/ DjangoModelPermissions(give add,change & delete permission to particular user)/ DjangoModelPermissionsOrAnonReadOnly]
     # filter_backends = [OrderingFilter]
     # ordering_fields = ['author'] 
 
